4078ac8e13186b1f6f8be6bd22f677b9d503553d
phanaswadikaratharva4-pixel <phanaswadikaratharva4@gmail.com>
2026-07-10 16:34:50 +0530

    Add files via upload

---
+++ PRACTICAL 2B.py
@@ -0,0 +1,14 @@
+a = int(input("enter first no:"))
+b = int(input("enter second no:"))
+
+c = a + b
+print("addition is",c)
+
+c = a - b
+print("subtraction is",c)
+
+c = a * b
+print("multiplication is",c)
+
+c = a / b
+print("division is",c)
