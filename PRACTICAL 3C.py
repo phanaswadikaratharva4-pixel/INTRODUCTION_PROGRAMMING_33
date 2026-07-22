db5b2126051bbb66c7a9c11f0915b2a4b3343696
phanaswadikaratharva4-pixel <phanaswadikaratharva4@gmail.com>
2026-07-22 10:37:15 +0530

    Add files via upload

---
+++ PRACTICAL 3C.py
@@ -0,0 +1,13 @@
+marks = float(input("enter marks:"))
+
+if marks >=90:
+    print("grade: A")
+elif marks >=75:
+    print("grade: B")
+elif marks >=50:
+    print("grade: C")
+else:
+    grade = "fail"
+    
+    print("Grade =",grade)    
+          
